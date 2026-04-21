409a4da2dabc0a3e764e9767ba746cdc1c499b34
Ander <66724762+anderhau@users.noreply.github.com>
2026-04-21 23:52:18 +0400

    Update image URLs to use i.imgur.com

--- src/data.ts
+++ src/data.ts
@@ -4,7 +4,7 @@ export const BOULDERS: Boulder[] = [
   {
     id: '1',
     name: 'Intro to Dyno',
-    imageUrl: 'https://imgur.com/FjUe9Ga',
+    imageUrl: 'https://i.imgur.com/FjUe9Ga.jpeg',
     fontGrade: '6A+ - 6B+',
     vGrade: 'V3-V4',
     date: '2026-04-21',
@@ -12,7 +12,7 @@ export const BOULDERS: Boulder[] = [
   {
     id: '2',
     name: 'Around the corner',
-    imageUrl: 'https://imgur.com/GIIY03R',
+    imageUrl: 'https://i.imgur.com/GIIY03R.jpeg',
     fontGrade: '6A+ - 6B+',
     vGrade: 'V3-V4',
     date: '2026-04-21',
@@ -20,7 +20,7 @@ export const BOULDERS: Boulder[] = [
   {
     id: '3',
     name: 'Run and Gun',
-    imageUrl: 'https://imgur.com/EpBi9zt',
+    imageUrl: 'https://i.imgur.com/EpBi9zt.jpeg',
     fontGrade: '6A+ - 6B+',
     vGrade: 'V3-V4',
     date: '2026-04-21',
@@ -28,7 +28,7 @@ export const BOULDERS: Boulder[] = [
   {
     id: '4',
     name: 'Tip Toe',
-    imageUrl: 'https://imgur.com/3fjfVbU',
+    imageUrl: 'https://i.imgur.com/3fjfVbU.jpeg',
     fontGrade: '6A+ - 6B+',
     vGrade: 'V3-V4',
     date: '2026-04-21',
@@ -36,7 +36,7 @@ export const BOULDERS: Boulder[] = [
   {
     id: '5',
     name: 'Wall ahead',
-    imageUrl: 'https://imgur.com/EkLiwmH',
+    imageUrl: 'https://i.imgur.com/EkLiwmH.jpeg',
     fontGrade: '6C+',
     vGrade: 'V5',
     date: '2026-04-21',
